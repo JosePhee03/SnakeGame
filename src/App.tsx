5374eeeac09bfd1447da342614c7d16e23f08f2c
JosePhee03 <tyttocr@gmail.com>
2023-03-13 14:57:06 -0300

    ✅ add app styles

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import {Arrow, ArrowBoard, GlobalStyle, Header, Main, Section} from './styles'
+import CanvasSnake from './components/canvas/Canvas'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <Main>
+        <Section>
+          <Header>
+            <h1>Snake</h1>
+          </Header>
+          <CanvasSnake/>
+          <ArrowBoard>
+            <Arrow>Up</Arrow>
+            <Arrow>Down</Arrow>
+            <Arrow>Left</Arrow>
+            <Arrow>Rigth</Arrow>
+          </ArrowBoard>
+        </Section>
+
+      </Main>
+    </>
+  )
+}
+
+export default App
+
+
+
