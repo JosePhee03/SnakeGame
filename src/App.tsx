7472afbb15713d9f3a75acf66c9eaa353f78292c
JosePhee03 <tyttocr@gmail.com>
2023-03-14 16:37:28 -0300

    ✨ Add ArrowIcon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Arrow, ArrowBoard, GlobalStyle, Header, Main, Section } from './styles'
 import CanvasSnake from './components/canvas/CanvasSnake'
+import { ArrowIcon } from './components/Icons'
 
 function App (): JSX.Element {
   return (
@@ -12,10 +13,10 @@ function App (): JSX.Element {
           </Header>
           <CanvasSnake />
           <ArrowBoard>
-            <Arrow>Up</Arrow>
-            <Arrow>Down</Arrow>
-            <Arrow>Left</Arrow>
-            <Arrow>Rigth</Arrow>
+            <Arrow><ArrowIcon direction='up' /></Arrow>
+            <Arrow><ArrowIcon direction='down' /></Arrow>
+            <Arrow><ArrowIcon direction='left' /></Arrow>
+            <Arrow><ArrowIcon direction='right' /></Arrow>
           </ArrowBoard>
         </Section>
 
