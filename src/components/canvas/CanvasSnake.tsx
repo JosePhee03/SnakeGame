970e0cc61fbfc2c1f10205d9eb31a9a7155ad47b
JosePhee03 <tyttocr@gmail.com>
2023-03-14 13:35:41 -0300

    ✨ add new feature bodySnake

--- src/components/canvas/CanvasSnake.tsx
+++ src/components/canvas/CanvasSnake.tsx
@@ -1,24 +1,50 @@
 import styled from 'styled-components'
 import { Canvas, SnakeHead } from '../../styles'
 import { useMoveSnake } from '../../hooks/useMoveSnake'
+import { useEffect, useState } from 'react'
+import AddBody from '../AddBody'
 
 function CanvasSnake (): JSX.Element {
-  const { snakeX, snakeY } = useMoveSnake()
+  const INITIAL_VALUE = [{ x: 0, y: 0 }]
 
-  console.log('snakeX', snakeX)
+  const { snakeX, snakeY } = useMoveSnake()
+  const [body, setBody] = useState(INITIAL_VALUE)
+
+  useEffect(() => {
+    let newBody = []
+
+    const beforeBody = body
+
+    newBody = body.map((coord, index) => (
+
+      index === 0
+        ? { ...coord, x: snakeX, y: snakeY }
+        : { ...coord, x: beforeBody[index - 1].x, y: beforeBody[index - 1].y }
+    ))
+
+    if (snakeX === 300 && snakeY === 300) {
+      AddBody(newBody)
+    }
+
+    setBody(newBody)
+  }, [snakeX, snakeY])
 
   return (
     <>
       <Canvas>
-        <SnakeHead
-          snakeY={snakeY}
-          snakeX={snakeX}
-        />
+        {body.map((coord, index) => (
+          <SnakeHead
+            key={index}
+            snakeX={coord.x}
+            snakeY={coord.y}
+          />
 
+        ))}
       </Canvas>
       <Footer>
         <h6>SnakeX: {snakeX}</h6>
         <h6>SnakeY: {snakeY}</h6>
+        <h6>body: {body.length}</h6>
       </Footer>
     </>
   )
