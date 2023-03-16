ed01353003f115975b6dd52fffc4143d45799da1
JosePhee03 <tyttocr@gmail.com>
2023-03-16 01:50:31 -0300

    ✅ Create MoveSnake

---
+++ src/components/MoveSnake.ts
@@ -0,0 +1,17 @@
+import { BodyType, KeyTypes, SnakeCoords } from '../types/types'
+
+export function MoveSnake (body: BodyType, key: KeyTypes): SnakeCoords {
+  const snakeHead = body[0]
+  const { snakeX, snakeY } = snakeHead
+
+  switch (key) {
+    case 'ArrowDown':
+      return ({ snakeX, snakeY: snakeY + 30 })
+    case 'ArrowUp':
+      return ({ snakeX, snakeY: snakeY - 30 })
+    case 'ArrowLeft':
+      return ({ snakeY, snakeX: snakeX - 30 })
+    case 'ArrowRight':
+      return ({ snakeY, snakeX: snakeX + 30 })
+  }
+}
