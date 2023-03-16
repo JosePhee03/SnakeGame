456f8528ff952f72d8a01336824640cf1adc4ee2
JosePhee03 <tyttocr@gmail.com>
2023-03-16 01:45:08 -0300

    ✅ Add MoveSnake

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { Arrow, ArrowBoard, GlobalStyle, Header, Main, Section } from './styles/styles'
 import CanvasSnake from './components/canvas/CanvasSnake'
 import { ArrowIcon } from './components/Icons'
-import { BodyProvider } from './context/bodyContext'
+import { useContext, useEffect } from 'react'
+import SnakeContext, { SnakeContextType } from './context/SnakeContext'
+import { MoveSnake } from './components/MoveSnake'
+import { KeyTypes } from './types/types'
+import AddBody from './components/AddBody'
 
 function App (): JSX.Element {
+  const { Snake, dispatch } = useContext(SnakeContext) as SnakeContextType
+  const { body } = Snake
+  const { snakeX, snakeY } = body[0]
+
+  const moveDown = (key: KeyTypes): void => {
+    const newSnakeHead = MoveSnake(body, key)
+    const newbody = body.map((e, i) => i === 0 ? newSnakeHead : e)
+    dispatch({ type: 'MOVE', payload: newbody })
+  }
+
+  useEffect(() => {
+    if (snakeX === 300 && snakeY === 300) {
+      const newBody = AddBody(body)
+      dispatch({ type: 'MOVE', payload: newBody })
+    }
+  }, [snakeX, snakeY])
+
   return (
-    <BodyProvider>
+    <>
       <GlobalStyle />
       <Main>
         <Section>
@@ -14,14 +35,14 @@ function App (): JSX.Element {
           </Header>
           <CanvasSnake />
           <ArrowBoard>
-            <Arrow><ArrowIcon direction='ArrowUp' /></Arrow>
-            <Arrow><ArrowIcon direction='ArrowDown' /></Arrow>
-            <Arrow><ArrowIcon direction='ArrowLeft' /></Arrow>
-            <Arrow><ArrowIcon direction='ArrowRight' /></Arrow>
+            <Arrow onClick={() => moveDown('ArrowUp')}><ArrowIcon direction='ArrowUp' /></Arrow>
+            <Arrow onClick={() => moveDown('ArrowDown')}><ArrowIcon direction='ArrowDown' /></Arrow>
+            <Arrow onClick={() => moveDown('ArrowLeft')}><ArrowIcon direction='ArrowLeft' /></Arrow>
+            <Arrow onClick={() => moveDown('ArrowRight')}><ArrowIcon direction='ArrowRight' /></Arrow>
           </ArrowBoard>
         </Section>
       </Main>
-    </BodyProvider>
+    </>
   )
 }
 
