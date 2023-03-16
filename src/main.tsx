2255e6d24c8f46119e4c672593707076ab7fe088
JosePhee03 <tyttocr@gmail.com>
2023-03-16 01:45:28 -0300

    ✅ Add SnakeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import SnakeProvider from './context/SnakeProvider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <>
+  <SnakeProvider>
     <App />
-  </>
+  </SnakeProvider>
 )
