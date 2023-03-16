581d94959bf4bec41cda1bc84ca93f98ab6b01e3
JosePhee03 <tyttocr@gmail.com>
2023-03-16 01:41:15 -0300

    ✅ Create SnakeProvider

---
+++ src/context/SnakeProvider.tsx
@@ -0,0 +1,22 @@
+import { useReducer } from 'react'
+import { SnakeType } from '../types/types'
+import SnakeContext from './SnakeContext'
+import SnakeReducer from './SnakeReducer'
+
+interface providerProps {
+  children: JSX.Element | JSX.Element[]
+}
+
+const INITIAL_VALUE: SnakeType = { status: 'START', body: [{ snakeX: 0, snakeY: 0 }], direction: 'ArrowRight' }
+
+function SnakeProvider ({ children }: providerProps): JSX.Element {
+  const [Snake, dispatch] = useReducer(SnakeReducer, INITIAL_VALUE)
+
+  return (
+    <SnakeContext.Provider value={{ Snake, dispatch }}>
+      {children}
+    </SnakeContext.Provider>
+  )
+}
+
+export default SnakeProvider
