39ffebb042c76652e53ca6572f615efa561ba84e
JosePhee03 <tyttocr@gmail.com>
2023-03-16 01:43:55 -0300

    ✅ Add body with SnakeContext

--- src/components/canvas/CanvasSnake.tsx
+++ src/components/canvas/CanvasSnake.tsx
@@ -1,32 +1,12 @@
 import styled from 'styled-components'
 import { Canvas, SnakeHead } from '../../styles/styles'
-import { useMoveSnake } from '../../hooks/useMoveSnake'
-import { useContext, useEffect } from 'react'
-import { BodyContext, bodyContextType } from '../../context/bodyContext'
+import { useContext } from 'react'
+import SnakeContext, { SnakeContextType } from '../../context/SnakeContext'
 
 function CanvasSnake (): JSX.Element {
-  const { body, setBody } = useContext(BodyContext) as bodyContextType
-  const { snakeX, snakeY } = useMoveSnake(body)
-
-  useEffect(() => {
-    let newBody = body
-
-    const beforeBody = body
-
-    newBody = body.map((coord, index) => (
-
-      index === 0
-        ? { ...coord, snakeY, snakeX }
-        : { ...coord, snakeX: beforeBody[index - 1].snakeX, snakeY: beforeBody[index - 1].snakeY }
-    ))
-
-    if (snakeX === 300 && snakeY === 300) {
-      newBody.push({ snakeX: -30, snakeY: -30 })
-      setBody(newBody)
-    } else {
-      setBody(newBody)
-    }
-  }, [snakeX, snakeY])
+  const { Snake } = useContext(SnakeContext) as SnakeContextType
+  const { body } = Snake
+  const { snakeX, snakeY } = body[0]
 
   return (
     <>
@@ -43,7 +23,7 @@ function CanvasSnake (): JSX.Element {
       <Footer>
         <h6>SnakeX: {snakeX}</h6>
         <h6>SnakeY: {snakeY}</h6>
-        <h6>body: {body.length}</h6>
+        <h6>body: {JSON.stringify(body)}</h6>
       </Footer>
     </>
   )
@@ -55,7 +35,5 @@ const Footer = styled.footer`
   position: absolute;
   top: 0;
   right: 0;
-  width: 100px;
-  height: 100px;
   background-color: beige;
 `
