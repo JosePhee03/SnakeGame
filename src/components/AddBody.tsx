769fef6bf603c10a3946c3e9d67036aeb448e74f
JosePhee03 <tyttocr@gmail.com>
2023-03-17 22:12:20 -0300

    🚚 Rename .tsx to .ts

--- src/components/AddBody.tsx
+++
@@ -1,9 +0,0 @@
-import { BodyType } from '../types/types'
-
-function AddBody (body: BodyType): BodyType {
-  const newBody = [...body]
-  newBody.push({ snakeX: -30, snakeY: -30 })
-  return newBody
-}
-
-export default AddBody
