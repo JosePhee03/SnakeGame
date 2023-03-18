df3a257002ec714fa526dc7b1e043e06e3a97a2c
JosePhee03 <tyttocr@gmail.com>
2023-03-18 16:10:11 -0300

    ✅ Update test with snakeSize variable

--- src/components/snakeHead.test.ts
+++ src/components/snakeHead.test.ts
@@ -1,6 +1,7 @@
+import { snakeSize } from '../styles/styles'
 import { snakeHead } from './snakeHead'
 
 test('the snake moves with the arrows', () => {
   expect(snakeHead([{ snakeX: 0, snakeY: 0 }], 'ArrowDown'))
-    .toStrictEqual({ snakeX: 0, snakeY: 30 })
+    .toStrictEqual({ snakeX: 0, snakeY: snakeSize })
 })
