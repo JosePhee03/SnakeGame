482324bcc3f4fb0a2dd1d7a5fbed5c342b296cff
JosePhee03 <tyttocr@gmail.com>
2023-03-17 22:02:26 -0300

    🔥 Remove MoveSnake test

--- src/components/MoveSnake.test.ts
+++
@@ -1,6 +0,0 @@
-import { MoveSnake } from './MoveSnake'
-
-test('the snake moves with the arrows', () => {
-  expect(MoveSnake([{ snakeX: 0, snakeY: 0 }], 'ArrowDown'))
-    .toStrictEqual({ snakeX: 0, snakeY: 30 })
-})
