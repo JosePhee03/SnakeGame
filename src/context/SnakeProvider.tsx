99e0830ffa1b1304bc4d7a5f1922aef52c45ac27
JosePhee03 <tyttocr@gmail.com>
2023-03-19 20:18:37 -0300

    ✅ Add export to the const INITIAL_VALUE

--- src/context/SnakeProvider.tsx
+++ src/context/SnakeProvider.tsx
@@ -7,7 +7,7 @@ interface providerProps {
   children: JSX.Element | JSX.Element[]
 }
 
-const INITIAL_VALUE: SnakeType = { status: 'START', body: [{ snakeX: 0, snakeY: 0 }], direction: 'ArrowRight' }
+export const INITIAL_VALUE: SnakeType = { status: 'START', body: [{ snakeX: 0, snakeY: 0 }], direction: 'ArrowRight' }
 
 function SnakeProvider ({ children }: providerProps): JSX.Element {
   const [Snake, dispatch] = useReducer(SnakeReducer, INITIAL_VALUE)
