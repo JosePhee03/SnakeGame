da4c98d476a1c6b2976ee78373249462eef46d82
JosePhee03 <tyttocr@gmail.com>
2023-03-20 20:56:34 -0300

    ✅ Add new Style neon from project

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -2,6 +2,18 @@ import styled, { createGlobalStyle } from 'styled-components'
 
 export const snakeSize = 32
 
+const COLORS = {
+  yellow: '#FDF800',
+  cyan: '#00E4F2',
+  pink: '#F000F2',
+  black: '#000000',
+  white: '#FFFFFF'
+}
+
+export const Svg = styled.svg`
+  filter: drop-shadow(0 0 4px ${COLORS.white});
+`
+
 export const Arrow = styled.div`
   width: 100%;
   height: 100%;
@@ -10,8 +22,12 @@ export const Arrow = styled.div`
   align-items: center;
 
   :hover{
-    background: white;
+    background: #ffffff15;
     cursor: pointer;
+  }
+
+  :hover > svg > path{
+    stroke: ${COLORS.yellow};
   }
 `
 
@@ -20,27 +36,61 @@ interface snakeCords {
   snakeY: number
 }
 
+export const Modal = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  position: absolute;
+  z-index: 20;
+  
+  h2 {
+    font-family: 'Press Start 2P', cursive;
+    font-size: 18px;
+    text-shadow: 0 0 4px white;
+    color: #fff;
+  };
+`
+
+export const ButtonGame = styled.button`
+    background: none;
+    border: solid 2px white;
+    font-family: 'Press Start 2P', cursive;
+    font-size: 12px;
+    text-shadow: 0 0 4px white;
+    color: #fff;
+    border-radius: 5px;
+    padding: 6px 12px;
+    box-shadow: 0 0 4px white, inset 0 0 4px white;
+  
+  :hover{
+    cursor: pointer;
+    background: #ffffff15;
+  }
+`
+
 export const SnakeHead = styled.div<snakeCords>`
   width: ${snakeSize}px;
   height: ${snakeSize}px;
   position: absolute;
   top: ${props => props.snakeY}px;
   left: ${props => props.snakeX}px;
-  background: red;
+  background: ${COLORS.pink};
+  filter: drop-shadow(0 0 4px ${COLORS.pink});
 `
 
 export const ArrowBoard = styled.div`
   width: 100%;
   height: 60px;
   display: flex;
-  background: #fdf800;
 `
 
 export const Canvas = styled.div`
   width: calc(${snakeSize}*12px);
   height: calc(${snakeSize}*12px);
   position: relative;
-  background: rgba(0, 0, 0, 0.3);
+  background-image: url(/pattern.svg);
+  box-shadow: inset 0 0 5px ${COLORS.white};
 `
 
 export const Header = styled.header`
@@ -48,7 +98,25 @@ export const Header = styled.header`
   justify-content: center;
   align-items: center;
   height: 60px;
-  background: #444;
+
+  h1 {
+    font-family: 'Lobster', cursive;
+    font-size: 30px;
+  }
+`
+
+export const Logo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  transform: translateY(-18px);
+
+  h1 {
+    color: white;
+    transform: translateY(-15px);
+    text-shadow: 0 0 10px white;
+  }
 `
 
 export const Main = styled.main`
@@ -56,21 +124,23 @@ export const Main = styled.main`
   height: 100vh;
   display: grid;
   place-content: center;
-  background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='60' height='60' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 0%, 1)'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5' transform='translate(10,0)' stroke-width='1' stroke='none' fill='hsla(0, 0%, 100%, 0.1450980392156863)'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")
+  background: #000;
 `
 
 export const Section = styled.section`
+  position: relative;
   border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: solid 5px ${COLORS.cyan};
+  box-shadow: 0px 0px 20px ${COLORS.cyan};
 `
 
 export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-  }
-
-  h1 {
-    color: #aaa;
-    font-weight: bolder;
   }
 `
