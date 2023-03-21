3c5cbaf4b9210fb3e853705a2be3a1c89586b10b
JosePhee03 <tyttocr@gmail.com>
2023-03-21 01:00:43 -0300

    ✅ Add status to ModalGame

--- src/components/ModalGame.tsx
+++ src/components/ModalGame.tsx
@@ -1,10 +1,16 @@
 import { ButtonGame, Modal } from '../styles/styles'
+import { StatusType } from '../types/types'
 
-function ModalGame (): JSX.Element {
+function ModalGame ({ status, handleStart }: { status: StatusType | null, handleStart: () => void }): JSX.Element {
   return (
     <Modal>
-      <h2>Start Game</h2>
-      <ButtonGame>Start</ButtonGame>
+      <h2>{status === 'START'
+        ? 'Start Game'
+        : status === 'GAME_OVER'
+          ? 'GameOver'
+          : ''}
+      </h2>
+      <ButtonGame onClick={handleStart}>Start</ButtonGame>
     </Modal>
   )
 }
