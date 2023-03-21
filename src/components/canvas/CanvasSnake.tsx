0c21567ecd3750f78ca3142dffea94f7e2b3cf49
JosePhee03 <tyttocr@gmail.com>
2023-03-21 01:02:02 -0300

    ✅ Add useContext and game over condition to CanvasSnake.

--- src/components/canvas/CanvasSnake.tsx
+++ src/components/canvas/CanvasSnake.tsx
@@ -1,22 +1,33 @@
 import { Canvas, SnakeHead } from '../../styles/styles'
 import { useContext } from 'react'
 import SnakeContext, { SnakeContextType } from '../../context/SnakeContext'
+import ModalGame from '../ModalGame'
 
 function CanvasSnake (): JSX.Element {
-  const { Snake } = useContext(SnakeContext) as SnakeContextType
-  const { body } = Snake
+  const { Snake, dispatch } = useContext(SnakeContext) as SnakeContextType
+  const { body, status } = Snake
+  const { snakeX, snakeY } = body[0]
+
+  const handleStart = (): void => {
+    dispatch({ type: 'START' })
+  }
+
+  if (snakeX < 0 || snakeX > 352 || snakeY < 0 || snakeY > 352) {
+    dispatch({ type: 'GAME_OVER' })
+  }
 
   return (
     <>
       <Canvas>
-        {body.map((coord, index) => (
-          <SnakeHead
-            key={index}
-            snakeX={coord.snakeX}
-            snakeY={coord.snakeY}
-          />
-
-        ))}
+        {status === 'START'
+          ? body.map((coord, index) => (
+            <SnakeHead
+              key={index}
+              snakeX={coord.snakeX}
+              snakeY={coord.snakeY}
+            />
+          ))
+          : <ModalGame status={status} handleStart={handleStart} />}
       </Canvas>
     </>
   )
