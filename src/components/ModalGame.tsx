0ce63e51c785149ab0bfe47f1370e1b13acbc342
JosePhee03 <tyttocr@gmail.com>
2023-03-20 21:07:57 -0300

    ✅ Create ModalGame

---
+++ src/components/ModalGame.tsx
@@ -0,0 +1,12 @@
+import { ButtonGame, Modal } from '../styles/styles'
+
+function ModalGame (): JSX.Element {
+  return (
+    <Modal>
+      <h2>Start Game</h2>
+      <ButtonGame>Start</ButtonGame>
+    </Modal>
+  )
+}
+
+export default ModalGame
