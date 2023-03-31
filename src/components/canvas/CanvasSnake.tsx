9c0c5be307e5409000537af623e34df07b206a1a
JosePhee03 <tyttocr@gmail.com>
2023-03-31 19:41:26 -0300

    ✅ Add useEffect and handleButton to CanvasSnake

--- src/components/canvas/CanvasSnake.tsx
+++ src/components/canvas/CanvasSnake.tsx
@@ -1,15 +1,34 @@
 import { Canvas, SnakeHead } from '../../styles/styles'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import SnakeContext, { SnakeContextType } from '../../context/SnakeContext'
 import ModalGame from '../ModalGame'
+import moveSnake from '../moveSnake'
+import useAddBody from '../../hooks/useAddBody'
+import { KeyTypes } from '../../types/types'
 
 function CanvasSnake (): JSX.Element {
   const { Snake, dispatch } = useContext(SnakeContext) as SnakeContextType
-  const { body, status } = Snake
+  const { body, status, direction } = Snake
   const { snakeX, snakeY } = body[0]
 
-  const handleStart = (): void => {
-    dispatch({ type: 'START' })
+  useAddBody(body, dispatch)
+
+  let moveInterval: NodeJS.Timer
+  useEffect(() => {
+    if (status === 'START') {
+      moveInterval = setInterval(() => {
+        const { newBody } = moveSnake(direction as KeyTypes, body)
+        dispatch({ type: 'MOVE', payload: newBody })
+        console.log(direction)
+      }, 200)
+    }
+    return () => clearInterval(moveInterval)
+  }, [body])
+
+  const handleButton = (): void => {
+    status === 'PAUSE'
+      ? dispatch({ type: 'START' })
+      : dispatch({ type: 'RESET' })
   }
 
   if (snakeX < 0 || snakeX > 352 || snakeY < 0 || snakeY > 352) {
@@ -19,15 +38,14 @@ function CanvasSnake (): JSX.Element {
   return (
     <>
       <Canvas>
-        {status === 'START'
-          ? body.map((coord, index) => (
-            <SnakeHead
-              key={index}
-              snakeX={coord.snakeX}
-              snakeY={coord.snakeY}
-            />
-          ))
-          : <ModalGame status={status} handleStart={handleStart} />}
+        {body.map((coord, index) => (
+          <SnakeHead
+            key={index}
+            snakeX={coord.snakeX}
+            snakeY={coord.snakeY}
+          />
+        ))}
+        {status !== 'START' && <ModalGame status={status} handleButton={handleButton} />}
       </Canvas>
     </>
   )
