26b5b2b11becf62bc45d7963c5089f600629efab
JosePhee03 <tyttocr@gmail.com>
2023-04-08 13:46:51 -0300

    ♻️ refactor types

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,10 +14,10 @@ export interface SnakeCoords { snakeX: number, snakeY: number }
 
 export type BodyType = SnakeCoords[]
 
+export interface FoodType { foodX: number, foodY: number }
 export interface SnakeType {
   status: StatusType | null
   body: BodyType
   direction: KeyTypes | null
+  food: FoodType
 }
-
-export interface FoodType { foodX: number, foodY: number }
