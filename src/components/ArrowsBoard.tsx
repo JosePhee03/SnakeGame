8d42ea7818025f25afbb38cd9d671a298e4657d2
JosePhee03 <tyttocr@gmail.com>
2023-04-10 01:16:15 -0300

    ♻️ Refactor and add useMemo

--- src/components/ArrowsBoard.tsx
+++ src/components/ArrowsBoard.tsx
@@ -1,18 +1,37 @@
-import useHandleMove from '../hooks/usehandleMove'
+import { useEffect, useMemo } from 'react'
 import { Arrow, Board } from '../styles/styles'
 import { KeyTypes } from '../types/types'
 import { ArrowIcon } from './Icons'
+import useSnake from '../hooks/useSnake'
 
 function ArrowsBoard (): JSX.Element {
-  const { pressArrow } = useHandleMove()
-  return (
+  const { Snake, userPressArrowDispatch } = useSnake()
+  const { direction } = Snake
+
+  const pressArrow = (key: KeyTypes): void => userPressArrowDispatch(key)
+
+  const keyPressEvent = (event: KeyboardEvent): void => {
+    const eventKey = event.key
+    const keys: KeyTypes[] = [KeyTypes.ArrowUp, KeyTypes.ArrowDown, KeyTypes.ArrowLeft, KeyTypes.ArrowRight]
+    const key = keys.find(e => e === eventKey)
+    if (key !== undefined) return pressArrow(key)
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyPressEvent)
+    return () => {
+      window.removeEventListener('keydown', keyPressEvent)
+    }
+  }, [direction])
+
+  return useMemo(() => (
     <Board>
       <Arrow onClick={() => pressArrow(KeyTypes.ArrowUp)}><ArrowIcon direction={KeyTypes.ArrowUp} /></Arrow>
       <Arrow onClick={() => pressArrow(KeyTypes.ArrowDown)}><ArrowIcon direction={KeyTypes.ArrowDown} /></Arrow>
       <Arrow onClick={() => pressArrow(KeyTypes.ArrowLeft)}><ArrowIcon direction={KeyTypes.ArrowLeft} /></Arrow>
       <Arrow onClick={() => pressArrow(KeyTypes.ArrowRight)}><ArrowIcon direction={KeyTypes.ArrowRight} /></Arrow>
     </Board>
-  )
+  ), [])
 }
 
 export default ArrowsBoard
--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,76 +1,36 @@
-import styled from 'styled-components'
 import { Logo, Header } from '../styles/styles'
-import { CrownLogo, PauseIcon } from './Icons'
+import { CrownLogo } from './Icons'
+import ScoreBoard from './ScoreBoard'
+import ButtonPause from './ButtonPause'
+import { useMemo } from 'react'
 import useSnake from '../hooks/useSnake'
 
 function HeaderMenu (): JSX.Element {
   const { Snake, pauseGameDispatch } = useSnake()
-  const { body, status } = Snake
+  const { status, body, food } = Snake
 
-  const handlePause = (): void => pauseGameDispatch(Snake)
+  const buttonPause = useMemo(() => (
+    <ButtonPause status={status} pauseGameDispatch={pauseGameDispatch} />
+  ), [status])
 
-  const score = body.length - 1
-  const padScore = score.toString().padStart(3, '000')
+  const scoreBoard = useMemo(() => (
+    <ScoreBoard body={body} food={food} />
+  ), [food, status])
+
+  const logo = useMemo(() => (
+    <Logo>
+      <CrownLogo />
+      <h1>Snake</h1>
+    </Logo>
+  ), [])
 
   return (
     <Header>
-      <Score>
-        <div>
-          <h6>Score</h6>
-          <h5>{padScore}</h5>
-        </div>
-      </Score>
-      <Logo>
-        <CrownLogo />
-        <h1>Snake</h1>
-      </Logo>
-      <HeaderButton>
-        {status === 'START' &&
-          <button onClick={handlePause}>
-            <PauseIcon />
-          </button>}
-      </HeaderButton>
+      {scoreBoard}
+      {logo}
+      {buttonPause}
     </Header>
   )
 }
 
 export default HeaderMenu
-
-const Score = styled.div`
-  display: flex;
-  padding-left: 20px;
-  
-  div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-shadow: 0 0 4px white;
-    font-family: 'Press Start 2P', cursive;
-    color: white;
-  }
-  
-  h6 {
-    font-size: 12px;
-  }
-  
-  h5 {
-    font-size: 16px;
-  }
-`
-
-const HeaderButton = styled.div`
-  display: flex;
-  justify-content: end;
-
-  button {
-    background: none;
-    border: none;
-    width: 32px;
-    height: 32px;
-    border-radius: 5px;
-    :hover {
-      cursor: pointer;
-      background: #ffffff50;
-    }
-  }
-`
